b8f6eef7606ee5b33e1dce8fddf76e684e51c8a4
sabooriftikhar <abdulsabooriftikhar3718@gmial.com>
2026-07-07 01:50:58 +0500

    api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,13 +1,22 @@
-import axios from "axios";
-
-const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-
-console.log("================================");
-console.log("NEXT_PUBLIC_API_URL =", BASE_URL);
-console.log("================================");
+import axios from 'axios';
 
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem('access_token');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
